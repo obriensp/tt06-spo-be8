6832a3c9194e78eb5955a5ba1c5e1a13651ef726
Sean Patrick O'Brien <sean.patrick.obrien@me.com>
2024-03-29 18:50:38 -0700

    Update tests

--- test/tb.v
+++ test/tb.v
@@ -1,4 +1,5 @@
-`default_nettype none `timescale 1ns / 1ps
+`timescale 1ns / 1ps
+`default_nettype none
 
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
@@ -16,11 +17,17 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  // reg [7:0] ui_in;
+  // reg [7:0] uio_in;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+
+  reg scl_i;
+  reg sda_i;
+  wire scl_o = uio_out[2];
+  wire sda_o = uio_out[3];
+
 
   // Replace tt_um_example with your module name:
   tt_um_example user_project (
@@ -31,9 +38,9 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
+      .ui_in  (8'b0),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
+      .uio_in ({4'b0, sda_i, scl_i, 2'b00}),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
